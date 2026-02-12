3ed4b74fc2e39a81c8deebe7c3e2b6bcea34f7b5
Azus <aziz.mirzaraximov@gmail.com>
2026-02-12 22:40:19 +0500

    Add with shared variables config

---
+++ src/ai_playground/src/config.py
@@ -0,0 +1,35 @@
+JSON_MIN_PATH = "src/ai_playground/data/export.json"
+
+IMAGE_DIR = "src/ai_playground/data/images"
+
+DATASET_PATH = "src/ai_playground/data/dataset_processed"
+
+BASE_MODEL_ID = "./src/ai_playground/models/base_model" 
+
+V1_MODEL_ID = "./src/ai_playground/models/custom_v1/final"
+
+MODELS_DIR = "./src/ai_playground/models"
+
+LABELS = [
+    "O", 
+    "B-Section_Header", "I-Section_Header",
+    "B-Test_Context_Name", "I-Test_Context_Name",
+    "B-Test_Name", "I-Test_Name",
+    "B-Test_Value", "I-Test_Value",
+    "B-Test_Unit", "I-Test_Unit",
+    "B-Test_Norm", "I-Test_Norm",
+    "B-Patient_Name", "I-Patient_Name",
+    "B-Patient_DOB", "I-Patient_DOB",
+    "B-Patient_Weight", "I-Patient_Weight",
+    "B-Patient_Height", "I-Patient_Height",
+]
+
+LABEL_COLORS = {
+    "Table_Context": "darkblue",
+    "Section_Header": "red",
+    "Test_Name": "green",
+    "Test_Value": "orange",
+    "Test_Unit": "cyan",
+    "Patient_Name": "purple",
+    "Patient_ID": "grey"
+}

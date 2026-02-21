7ae70d0ba1ad8bc7df3cdd2513c834d03eb2a2cd
Azus <aziz.mirzaraximov@gmail.com>
2026-02-21 15:50:49 +0500

    feature: prediction

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import shutil
 from src.extraction.document import MedicalDocument
 from src.extraction.converter import DocumentConverter
 from src.extraction.ocr import TextExtractor
+from src.model.inference import LayoutLMPredictor
 from src.config import DATA_INPUT_PATH, DATA_OUTPUT_PATH, DATA_FAILED_PATH
 
 def main():
-    extractor = TextExtractor() 
+    extractor = TextExtractor()
+    predictor = LayoutLMPredictor() 
 
     os.makedirs(DATA_FAILED_PATH, exist_ok=True)
     
@@ -18,22 +20,24 @@ def main():
     for filename in incoming_files:
         file_path = os.path.join(DATA_INPUT_PATH, filename)
         
-        try:
-            print(f"\n--- Processing: {filename} ---")
-            
-            doc = MedicalDocument(file_path)
-            DocumentConverter.convert_to_images(doc)
-            extractor.extract(doc)
-            
-            # processed_path = os.path.join(DATA_OUTPUT_PATH, doc.filename)
-            # shutil.move(doc.original_path, processed_path)
-            # print(f"✅ Successfully processed and archived: {doc.filename}")
-            
-        except Exception as e:
-            print(f"❌ Failed to process {filename}. Error: {e}")
-            
-            failed_path = os.path.join(DATA_FAILED_PATH, filename)
-            shutil.move(file_path, failed_path)
+        print(f"\n--- Processing: {filename} ---")
+        
+        doc = MedicalDocument(file_path)
+        DocumentConverter.convert_to_images(doc)
+        extractor.extract(doc)
+        predictor.predict(doc)
 
+        for page in doc.extracted_data:
+            for token in page:
+                if "label" in token:
+                    print(f"Found: {token['text']} -> {token['label']} ({token['confidence']:.2f})")
+                else:
+                    print(f"Found: {token['text']}, no label")
+        
+        # processed_path = os.path.join(DATA_OUTPUT_PATH, doc.filename)
+        # shutil.move(doc.original_path, processed_path)
+        # print(f"✅ Successfully processed and archived: {doc.filename}")
+            
+        
 if __name__ == "__main__":
     main()
